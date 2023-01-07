b75b256d09c72e6c9f82eeab5a43b22479778164
kifuan <kifuan@foxmail.com>
2023-01-07 16:03:41 +0800

    feat: cache specified keywords

--- app.py
+++ app.py
@@ -3,11 +3,25 @@ import random
 import uvicorn
 
 from pathlib import Path
-
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 
+from functools import cache
 
+
+# Uvicorn port.
+PORT = 8003
+
+# Keywords to cache.
+CACHED_KEYWORDS = ['原神', '压缩毛巾']
+
+# Cached datasets. It will be updated at runtime when matches keyword in `CACHED_KEYWORDS`
+CACHED_DATASETS = {}
+
+app = FastAPI()
+
+
+@cache
 def load_dataset() -> list[str]:
     path = Path(__file__).parent / 'data' / 'dataset.json'
     if not path.exists():
@@ -15,25 +29,28 @@ def load_dataset() -> list[str]:
     return ujson.loads(path.read_text('utf8'))
 
 
-# Uvicorn port.
-PORT = 8003
+def load_keyword_dataset(keyword: str) -> list[str]:
+    dataset = load_dataset()
 
-# All texts.
-DATASET = load_dataset()
+    if not keyword:
+        return dataset
 
+    if keyword in CACHED_DATASETS:
+        return CACHED_DATASETS[keyword]
 
-app = FastAPI()
+    dataset = [text for text in dataset if keyword in text]
+
+    # Cache it at the first time, and it will use cached data next time.
+    if keyword in CACHED_KEYWORDS:
+        CACHED_DATASETS[keyword] = dataset
+
+    return dataset
 
 
 @app.get('/')
 async def handle_text(keyword: str = ''):
-    if not keyword:
-        return random.choice(DATASET)
-
-    dataset = [text for text in DATASET if keyword in text]
-    if dataset:
+    if dataset := load_keyword_dataset(keyword):
         return random.choice(dataset)
-
     return PlainTextResponse(f'No text has matched keyword {keyword}', 404)
 
 if __name__ == '__main__':
